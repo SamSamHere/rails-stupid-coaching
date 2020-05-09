1c7c6d6ad80296ebe2f11fd5af23cd842a2057cd
Samantha Lee <samanthaanne.lee28@gmail.com>
2020-05-09 19:31:55 +0800

    Update. Asking function wokring for coach.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,17 +1,24 @@
 class QuestionsController < ApplicationController
 
   def ask
-    puts "What do you want to ask the Coach?"
   end
 
-  def ans
-    if question == 'I am going to work'
+  def answer
+    # puts params[:question]
+    @question = params[:question_asked].capitalize
+    @coach_answer = reply(@question)
+    # Print Questions and Answers on the View Page
+  end
+
+  def reply(input)
+    if input == 'I am going to work'
       'Great!'
-    elsif question.includes('?')
+    elsif input.include?("?")
       'Silly question, get dressed and go to work!'
     else
       "I don't care, get dressed and go to work!"
     end
+
   end
 
 end
